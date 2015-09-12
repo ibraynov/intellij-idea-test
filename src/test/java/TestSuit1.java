b8ed63cefe03de1c9e4dcc1a20500b5da4e07d46
Ivan <ivan.braynov@gmail.com>
2015-09-13 02:37:14 +0300

    5th commit. added some actual tests

--- src/test/java/TestSuit1.java
+++ src/test/java/TestSuit1.java
@@ -1,5 +1,31 @@
-/**
- * Created by ivan on 13.09.15.
- */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class TestSuit1 {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public  void validLogin(){
+        driver.manage().window().maximize();
+        driver.get("http://abv.bg");
+
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
 }
