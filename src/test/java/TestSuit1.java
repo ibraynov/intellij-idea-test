8bfaa2eb6295a8ea4d198b6f323b825a2289a8bd
Ivan <ivan.braynov@gmail.com>
2015-09-14 22:30:33 +0300

    Created Login Test

--- src/test/java/TestSuit1.java
+++ src/test/java/TestSuit1.java
@@ -1,10 +1,14 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestSuit1 {
 
@@ -18,14 +22,25 @@ public class TestSuit1 {
     }
 
     @Test
-    public  void validLogin(){
+    public void validLogin() {
         driver.manage().window().maximize();
         driver.get("http://abv.bg");
+        String username = "dev_provers@abv.bg";
+        String password = "provers2015";
+        String welcomeMessage = "("+username+")";
+        WebElement usernameField = driver.findElement(By.xpath(".//*[@id='username']"));
+        usernameField.sendKeys(username);
+        WebElement passwordField = driver.findElement(By.xpath(".//*[@id='password']"));
+        passwordField.sendKeys(password);
+        WebElement loginButton = driver.findElement(By.xpath(".//*[@id='loginBut']"));
+        loginButton.click();
 
+        assertEquals(welcomeMessage, driver.findElement(By.xpath(".//*[@id='middlePagePanel']/div[1]/div[2]")).getText());
     }
 
     @After
     public void tearDown(){
+
         driver.quit();
     }
 }
