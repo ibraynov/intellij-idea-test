6f5c30adfe17eb1c3390850db26d02624523eddb
Ivan <ivan.braynov@gmail.com>
2015-09-15 00:43:04 +0300

    Dropdown selection tests

--- src/test/java/TestSuite1.java
+++ src/test/java/TestSuite1.java
@@ -1,18 +1,21 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
-public class TestSuit1 {
+public class TestSuite1 {
 
     private WebDriver driver;
+    private Select dropDownSelector;
 
     @Before
     public void setUp(){
@@ -36,6 +39,22 @@ public class TestSuit1 {
         loginButton.click();
 
         assertEquals(welcomeMessage, driver.findElement(By.xpath(".//*[@id='middlePagePanel']/div[1]/div[2]")).getText());
+    }
+
+    @Test
+    public void hiddenDropDownSelect() throws InterruptedException {
+
+        driver.get("https://book.goindigo.in");
+        WebElement dropDown = driver.findElement(By.xpath(".//input[@id='txtOriginStationRound']"));
+        dropDown.click();
+        WebElement DropDownInner = driver.findElement(By.xpath("//div[3]/ul/li[27]/a/span"));
+        DropDownInner.click();
+        /*Thread.sleep(5000);
+        dropDownSelector = new Select(dropDown);
+        dropDownSelector.selectByVisibleText("Goa (GOI)");*/
+        Alert popup = driver.switchTo().alert();
+        popup.accept();
+
     }
 
     @After
